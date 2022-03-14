aa1dda41dc24af5acfb3bbfedab697dc534dae86
zMateusReis <99493482+zMateusReis@users.noreply.github.com>
2022-03-14 10:09:29 -0300

    commit inicial

---
+++ src/JS/index.js
@@ -0,0 +1,26 @@
+// precisamos criar duas variáveis no JS para trabalhar com os elementos da tela
+const listaSelecaoPokemons = document.querySelectorAll('.pokemon')
+const pokemonsCard = document.querySelectorAll('.cartao-pokemon')
+
+listaSelecaoPokemons.forEach(pokemon => {
+    pokemon.addEventListener('click', () => {
+    //remover a classe aberto só do cartão que está aberto
+    const cartaoPokemonAberto = document.querySelector('.aberto')
+    cartaoPokemonAberto.classList.remove('aberto')
+
+    // ao clicar em um pokemon da listagem pegamos o id desse pokemon pra saber qual cartão mostrar 
+    const idPokemonSelecionado = pokemon.attributes.id.value
+
+    const idDoCartaoPokemonParaAbrir = 'cartao-' + idPokemonSelecionado
+    const cartaoPokemonParaAbrir = document.getElementById(idDoCartaoPokemonParaAbrir)
+    cartaoPokemonParaAbrir.classList.add('aberto')
+
+    //remover a classe ativo que marca o pokemon selecionado
+    const pokemonAtivoNaListagem = document.querySelector('.ativo')
+    pokemonAtivoNaListagem.classList.remove('ativo')
+
+    //adicionar a classe ativo no item a lista selecionado
+    const pokemonSelecionadoNaListagem = document.getElementById(idPokemonSelecionado)
+    pokemonSelecionadoNaListagem.classList.add('ativo')
+    })
+})
